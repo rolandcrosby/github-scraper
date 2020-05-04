5b72e4821d1254dfd69935333ee324d83d713993
Roland Crosby <roland@rolandcrosby.com>
2020-05-04 16:24:12 +0000

    allow passing a port list on stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use github_v3::*;
+use github_v3::{Client, GHError};
 use serde_derive::*;
 use std::cmp::{max, min};
 use std::time::Duration;
+use std::io::{self, Read};
+use std::iter::IntoIterator;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Serp {
@@ -11,24 +13,39 @@ pub struct Serp {
 #[tokio::main]
 async fn main() -> Result<(), GHError> {
     let args: Vec<String> = std::env::args().collect();
-    let start: u32 = match args.len() {
-        1 => 1000,
-        _ => args[1].parse().unwrap_or(1000),
-    };
-    let end: u32 = match args.len() {
-        1 | 2 => 65535,
-        _ => args[2].parse().unwrap_or(65535),
-    };
+    match args.len() {
+        0 | 1 => {
+            if atty::is(atty::Stream::Stdin) {
+                run(1000..=65535).await
+            } else {
+                let mut buffer = String::new();
+                match io::stdin().read_to_string(&mut buffer) {
+                    Ok(n) if n > 0 => {
+                        run(buffer.split('\n').flat_map(|n| n.parse::<u32>().ok()).into_iter()).await
+                    },
+                    _ => run(1000..=65535).await
+                }
+            }
+        },
+        2 => run(1000..=args[1].parse().unwrap_or(65535)).await,
+        _ => run((args[1].parse().unwrap_or(1000))..=(args[2].parse().unwrap_or(65535))).await
+    }
+}
+
+async fn run<I>(mut ports: I) -> Result<(), GHError>
+where I: Iterator<Item=u32> {
     let gh = Client::new_from_env();
     let min_sleep = 4; // the published rate limit is 30 requests per minute
     let max_sleep = 600;
     let max_errors = 10;
-    let mut port = start;
-    let mut next_sleep = 2;
+
+    let mut item = ports.next();
     let mut consecutive_errors = 0;
-    while port <= end {
-        if port > 1000 || consecutive_errors > 0 {
-            std::thread::sleep(Duration::from_secs(next_sleep));
+    let mut next_sleep = 0;
+    while let Some(port) = item {
+        std::thread::sleep(Duration::from_secs(next_sleep));
+        if next_sleep < min_sleep {
+            next_sleep = min_sleep;
         }
         let query = format!("\"localhost:{}\"", port);
         let res = gh
@@ -40,12 +57,12 @@ async fn main() -> Result<(), GHError> {
             .await;
         if let Ok(res) = res {
             if let Ok(res) = res.obj::<Serp>().await {
-                println!("{},{}", query, res.total_count);
+                println!("{},{}", port, res.total_count);
                 if next_sleep > min_sleep {
                     next_sleep = max(min_sleep, next_sleep / 2);
                 }
                 consecutive_errors = 0;
-                port += 1;
+                item = ports.next();
                 continue;
             }
         }
@@ -59,8 +76,8 @@ async fn main() -> Result<(), GHError> {
         }
         consecutive_errors += 1;
         eprintln!(
-            "{} consecutive errors. sleeping for {} secs",
-            consecutive_errors, next_sleep
+            "[{}] {} consecutive errors. sleeping for {} secs",
+            port, consecutive_errors, next_sleep
         );
         if consecutive_errors > max_errors {
             break;
